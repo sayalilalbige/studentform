d033e146cd2c04b33bb3eb9ef9a26f39134dab87
sayali <lalbige.sayali31@gmail.com>
2018-10-16 13:09:48 +0530

    deploy application

---
+++ src/demo/demo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'demo-root',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.css']
+})
+export class DemoComponent {
+  title = 'My first angular component';
+}
